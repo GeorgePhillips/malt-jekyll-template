e972496beb7a26170a85242de9cb5cf468504328
Mike Neumegen <mike@cloudcannon.com>
2016-11-21 21:25:19 -0800

    firefox support & success page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-function init_map() {
+function getQueryStringParams(sParam) {
+	var sPageURL = window.location.search.substring(1);
+	var sURLVariables = sPageURL.split('&');
+
+	for (var i = 0; i < sURLVariables.length; i++) {
+		var sParameterName = sURLVariables[i].split('=');
+		if (sParameterName[0] == sParam) {
+			return sParameterName[1];
+		}
+	}
+}
+
+function initMap() {
 	var myOptions = {
 		scrollwheel: false,
 		draggable: false,
@@ -31,6 +43,10 @@ $(window).on("load", function() {
 		itemSelector: '.grid-item',
 		gutter: 10
 	});
+
+	if (getQueryStringParams('submit') === "success") {
+		$(".success").css("display", "block");
+	}
 });
 
-google.maps.event.addDomListener(window, 'load', init_map);
+google.maps.event.addDomListener(window, 'load', initMap);
